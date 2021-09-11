6e35a1372f11d4044004f431199b2557e4375a86
Julian Toscani <julian_toscani@gmx.de>
2021-09-11 17:22:19 +0200

    add route to read available files and serve files statically

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello!");
+import Koa from "koa";
+import serve from "koa-static";
+import Router from "koa-router";
+import mount from "koa-mount";
+import path from "path";
+import fs from "fs/promises";
+
+const app = new Koa();
+const staticApp = new Koa();
+const router = new Router();
+
+const PORT = 33332;
+
+staticApp.use(serve(path.join(__dirname, "../race_data")));
+
+app.use(mount("/downloads", staticApp));
+
+router.get("/", (ctx) => (ctx.body = "Hello you!"));
+
+router.get("/downloads", async (ctx) => {
+  const dir = await fs.readdir(path.resolve(__dirname, "../race_data"));
+  ctx.body = JSON.stringify(dir);
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`Listening on: ${PORT}`);
+});
