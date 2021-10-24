70c20438642336a28363c24b5480ce24a901ed4b
Julian Toscani <julian_toscani@gmx.de>
2021-10-24 22:25:21 +0200

    simplify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import Koa from "koa";
-import serve from "koa-static";
-import Router from "koa-router";
-import mount from "koa-mount";
-import path from "path";
-import fs from "fs/promises";
+import router from "./router.ts";
+
+import { connectDatabase } from "./db/connectDb";
 
 const app = new Koa();
-const staticApp = new Koa();
-const router = new Router();
 
-const PORT = 33332;
-
-staticApp.use(serve(path.join(__dirname, "../race_data")));
-
-app.use(mount("/downloads", staticApp));
-
-router.get("/", (ctx) => (ctx.body = "Hello you!"));
-
-router.get("/downloads", async (ctx) => {
-  const dir = await fs.readdir(path.resolve(__dirname, "../race_data"));
-  ctx.body = JSON.stringify(dir);
-});
+const PORT = 4444;
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(PORT, () => {
-  console.log(`Listening on: ${PORT}`);
+connectDatabase().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on: ${PORT}`);
+  });
 });
