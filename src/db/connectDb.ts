9d9499468fc0e72e62740eee911715a6173610fa
Julian Toscani <julian_toscani@gmx.de>
2021-10-25 22:52:02 +0200

    use dotenv

--- src/db/connectDb.ts
+++ src/db/connectDb.ts
@@ -1,11 +1,15 @@
 import { Collection, MongoClient } from "mongodb";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 let client: MongoClient;
 
 const DB_USER = process.env.DB_USER;
 const DB_PW = process.env.DB_PW;
+const DB_HOST = process.env.DB_HOST;
 
-const URL = `mongodb://${DB_USER}:${DB_PW}@fm7-db:27017/?authSource=admin`;
+const URL = `mongodb://${DB_USER}:${DB_PW}@${DB_HOST}:27017/?authSource=admin`;
 
 export async function connectDatabase(): Promise<void> {
   client = new MongoClient(URL);
