bca72676b2cf4b26942d1a66fb631679bda9f517
Julian Toscani <julian_toscani@gmx.de>
2021-10-26 23:05:47 +0200

    WIP use object to save data -> checking laps

--- src/app/saveAsLaps.ts
+++ src/app/saveAsLaps.ts
@@ -1,40 +1,39 @@
-import { RawRaceData } from "../db/repos/RaceDataRaw";
-import path from "path";
 import fs from "fs";
+
+import { RawRaceData } from "../db/repos/RaceDataRaw";
+import all from "../encoding/all";
 import decodeBuffer, { createDecoder } from "../encoding/decodeBuffer";
+import LapDataHandler from "./LapDataHandler";
 
 export default async function saveAsLaps(raceData: RawRaceData) {
-  const raceDataEntries = getRaceDataEntries(raceData);
+  const laps = new Set();
 
-  const lapData = getLapData(raceDataEntries);
+  const raceDataAsBuffer = getraceDataAsBuffer(raceData);
 
+  const config = {
+    hash: raceData.hash,
+    started: raceData.started,
+    lap: 1,
+  };
+
+  const handler = new LapDataHandler(config);
+  await fs.promises.mkdir(handler.pathToDir, { recursive: true });
+
+  handler.init();
+
+  for (let index = 0; index < raceDataAsBuffer.length; index++) {
+    const entry = raceDataAsBuffer[index];
+    const decoder = createDecoder(entry);
+    const decodedRaceData = decodeBuffer(entry, decoder);
+
+    laps.add(decodedRaceData[75]);
+    // handler.write(decodedRaceData);
+  }
+  console.log(laps);
   return true;
 }
 
-function getRaceDataEntries(raceData: RawRaceData) {
-  return raceData.data.map((data) => data[0].buffer);
-}
-
-function getLapData(raceDataEntries: Buffer[]) {
-  const decoder = createDecoder(raceDataEntries[0]);
-
-  const lapsData = [];
-  const lapData = [];
-  const currentLap = 1;
-
-  for (let index = 0; index < raceDataEntries.length; index++) {
-    const entry = raceDataEntries[index];
-    const decodedData = decodeBuffer(Buffer.from(entry), decoder);
-
-    if ()
-  }
-}
-
-async function getWriteStream(hash: string, lap: number) {
-  const pathToDir = path.resolve(__dirname, "../../race_data_files", hash);
-  await fs.promises.mkdir(pathToDir, { recursive: true });
-
-  return fs.createWriteStream(path.resolve(pathToDir, filename), {
-    flags: "a",
-  });
+function getraceDataAsBuffer(raceData: RawRaceData) {
+  const flatData = raceData.data.flat();
+  return flatData.map((data) => data.buffer);
 }
