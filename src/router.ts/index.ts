e0f9389d539454fbce75cb53cb74097b2373c492
Julian Toscani <julian_toscani@gmx.de>
2021-10-30 19:32:14 +0200

    add new route to save disk data to collection

--- src/router.ts/index.ts
+++ src/router.ts/index.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import Router from "koa-router";
 import RawRaceDataRepo from "../db/repos/RaceDataRaw";
 import rawToDiskLaps from "../app/rawToDiskLaps";
+import diskLapsToDB from "../app/diskLapsToDB";
 
 const router = new Router();
 
@@ -16,7 +17,21 @@ router.get("/raw-to-laps/:id", async (ctx) => {
   }
 
   await rawToDiskLaps(data);
-  ctx.response.body = { message: "Data saved successfuly!" };
+  ctx.response.body = { message: "Data saved successfuly to disk!" };
+});
+
+router.get("/disk-to-collection/:id", async (ctx) => {
+  const repo = await new RawRaceDataRepo();
+  const data = await repo.getOne({
+    _id: new ObjectId(ctx.params.id),
+  });
+
+  if (!data) {
+    return;
+  }
+
+  await diskLapsToDB(data);
+  ctx.response.body = { message: "Data saved successfuly to collection!" };
 });
 
 export default router;
