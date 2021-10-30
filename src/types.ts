97d2d7bebbffe7d4b5538061f3a2f56c0ec47ed3
Julian Toscani <julian_toscani@gmx.de>
2021-10-30 22:47:20 +0200

    add ctx type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import Application from "koa";
+import Router from "koa-router";
+
 import base from "./encoding/base";
 import all from "./encoding/all";
 
@@ -8,3 +11,9 @@ export type BufferKeysV1 = V1[number]["property"];
 export type BufferKeysV2 = V2[number]["property"];
 export type EncodingsV1 = V1[number]["encoding"];
 export type EncodingsV2 = V2[number]["encoding"];
+
+export type KoaContext = Application.ParameterizedContext<
+  any,
+  Router.IRouterParamContext<any, {}>,
+  any
+>;
